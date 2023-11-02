c4821e28dcca422f2daa59e728aaf0322b28f0a6
ivka-n <145652401+ivka-n@users.noreply.github.com>
2023-11-02 09:34:52 +0100

    Add files via upload

---
+++ Lecture 5/Lecture 5/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ForegroundColor = ConsoleColor.Cyan;
+
+
+
+//float AddOne(float x) //float - des čísla
+//{
+//    return x + 1;
+//}
+
+//void PrintNumber(float number)
+//{
+//    Console.WriteLine(number);
+
+//}
+
+
+
+//float x = 3.14f;
+//float y = AddOne(x);
+
+//Console.WriteLine(y);
+
+//float Maximum(float x, float y)
+//{
+//    return x > y ? x : y;
+//}
+
+//float x = 2;
+//float y = 2.00001f;
+//Console.WriteLine(Maximum(x, y));
+
+
+//int x = 0;
+//int y = 1;
+//float Maximum(float x, float y)
+//{
+//    if (x < y)
+//    { 
+//        Console.WriteLine(y);
+//        return y;
+//    }
+//    else if (x > y)
+//    {
+//        Console.WriteLine(x);
+//        return x;
+//    }
+//    else
+//    {
+//        Console.WriteLine(x + " = " + y);
+//        return x;
+//    }
+//}
+//Console.WriteLine(Maximum(x, y));
+
+
+//int number = 0;
+//int VstupOdUzivatele()    // funkce
+//{
+//    Console.WriteLine("Napiš číslo");
+//    string input = Console.ReadLine();     
+//    int.TryParse(input, out number);
+//    return number;
+//}
+
+
+//Console.WriteLine("Před funkcí");
+//int vstup = VstupOdUzivatele(); // tímhle se ta funkce "aktivuje"
+//Console.WriteLine("Po funkci");
+//Console.WriteLine(vstup + 1);
+
+double result = Math.Sqrt(4);
+Console.WriteLine(result);
